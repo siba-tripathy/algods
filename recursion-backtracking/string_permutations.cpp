534084ed8d0b492c04631e10f37cf1325e870080
Siba Prasad Tripathy <sibatripathy19@gmail.com>
2021-10-24 13:49:10 +0530

    adding most optimized approach

--- recursion-backtracking/string_permutations.cpp
+++ recursion-backtracking/string_permutations.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
 
-// this method will print all permutations, but if input string has duplicate chars, then sequences can be repeated
+// this method will print all permutations(Wrong), but if input string has duplicate chars, then sequences can be repeated
 void printAllPermutationsRepeated(string input, string ans) {
     if(input.empty()) {
         cout<<ans<<endl;
@@ -33,9 +34,27 @@ void printAllPermutations(string input, string ans) {
     }
 }
 
+//most optimized solution - Very imp to see that backtracknig(recovering aspect) is useful.
+void printAllPermutations_optimized(string input, int i) {
+    if(i == input.length()-1) {
+        cout<<input<<endl;
+        return;
+    }
+    vector<bool> visited = vector<bool>(26, false);
+    for(int j=i; j<input.length(); j++) {
+        if (!visited[input[j]-'a']) {
+            visited[input[j]-'a'] = true;
+            swap(input[j], input[i]);     //take each element to the ith place
+            printAllPermutations_optimized(input, i+1);
+            swap(input[j], input[i]);     //swap again to recover input string, so that next loop/ next child recursion has right input
+        }
+    }
+}
+
 
 int main() {
     string str;
     cin>>str;
-    printAllPermutations(str, "");
+    //printAllPermutations(str, "");
+    printAllPermutations_optimized(str, 0);
 }
