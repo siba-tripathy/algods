798090778367250c9e57d510fa81b00fa50d8872
Siba Prasad Tripathy <sibatripathy19@gmail.com>
2021-11-01 08:17:21 +0530

    added info comments

--- recursion-backtracking/n-queens.cpp
+++ recursion-backtracking/n-queens.cpp
@@ -8,7 +8,7 @@ bool isAttackedFromAbove(vector<vector<int> > &board, int x, int y) {
         return false;
     }
     int leftdiagonal = y-1, rightdiagonal = y+1;
-    for(int i = x-1; i>=0; i--) {
+    for(int i = x-1; i>=0; i--) {//check the ith row above i goes from x-1 to 0
         if (board[i][y] == 1) {
             return true;
         }
@@ -28,17 +28,18 @@ bool isAttackedFromAbove(vector<vector<int> > &board, int x, int y) {
     return false;
 }
 
+//part of backtracking : prune and recover the original
 int getNumberOfStables(vector<vector<int> > &board, int currentRow) {
 
     int possibleStables = 0;
     for(int j = 0; j< board.size(); j++) {
-        if(!isAttackedFromAbove(board, currentRow, j)) {
+        if(!isAttackedFromAbove(board, currentRow, j)) {           // prune
             if (currentRow == board.size()-1) {
                 possibleStables++;
             } else {
-                board[currentRow][j] = 1;
+                board[currentRow][j] = 1;                           //for child tree
                 possibleStables+=getNumberOfStables(board, currentRow+1);
-                board[currentRow][j] = 0;
+                board[currentRow][j] = 0;                           //recover the original
             }
         }
     }
